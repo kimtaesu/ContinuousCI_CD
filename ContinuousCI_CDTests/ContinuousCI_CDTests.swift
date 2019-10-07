261a26f12282a4ef6f762280b050e07ea281c318
kimtaesu <kimtaesoo188@gmail.com>
2019-10-07 17:50:07 +0900

    First commit

---
+++ ContinuousCI_CDTests/ContinuousCI_CDTests.swift
@@ -0,0 +1,34 @@
+//
+//  ContinuousCI_CDTests.swift
+//  ContinuousCI_CDTests
+//
+//  Created by tskim on 07/10/2019.
+//  Copyright © 2019 assin. All rights reserved.
+//
+
+import XCTest
+@testable import ContinuousCI_CD
+
+class ContinuousCI_CDTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ContinuousCI_CD' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ContinuousCI_CD
+
+  target 'ContinuousCI_CDTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ContinuousCI_CDUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
